9775fd524208a4bac3f7b15fcdaf7d790d2755af
Johann Schopplich <mail@johannschopplich.com>
2023-10-12 21:44:14 +0200

    feat!: update layout for Kirby 4

--- src/panel/components/PlausibleView.vue
+++ src/panel/components/PlausibleView.vue
@@ -1,25 +1,3 @@
-<template>
-  <k-inside>
-    <k-view class="k-plausible-view">
-      <k-header>Plausible Analytics</k-header>
-      <iframe
-        v-if="sharedLink"
-        :src="`${sharedLink}&embed=true&theme=light&background=transparent`"
-        plausible-embed
-        scrolling="no"
-        frameborder="0"
-        loading="lazy"
-      />
-      <k-box v-else theme="negative">
-        You need to set the <code>PLAUSIBLE_SHARED_LINK</code> environment
-        variable in your <code>.env</code> file or
-        <code>johannschopplich.plausible.sharedLink</code> in your
-        <code>config.php</code>.
-      </k-box>
-    </k-view>
-  </k-inside>
-</template>
-
 <script>
 export default {
   props: {
@@ -31,11 +9,35 @@ export default {
 };
 </script>
 
+<template>
+  <k-panel-inside class="k-plausible-view">
+    <k-header>Plausible Analytics</k-header>
+
+    <iframe
+      v-if="sharedLink"
+      :src="`${sharedLink}&embed=true&theme=light&background=transparent`"
+      plausible-embed
+      scrolling="no"
+      frameborder="0"
+      loading="lazy"
+    />
+    <k-box v-else theme="negative">
+      You need to set the <code>PLAUSIBLE_SHARED_LINK</code> environment
+      variable in your <code>.env</code> file or
+      <code>johannschopplich.plausible.sharedLink</code> in your
+      <code>config.php</code>.
+    </k-box>
+  </k-panel-inside>
+</template>
+
 <style scoped>
 iframe {
   width: 1px;
   min-width: 100%;
   height: 1700px;
+  /* Counterpart padding inside Plausible frame */
+  margin-left: -1.5rem;
+  margin-right: -1.5rem;
 }
 
 @media (max-width: 850px) {
